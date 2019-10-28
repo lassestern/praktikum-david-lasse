b225493cb86f21d26f7068603b54ab9d31f7c120
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2019-10-28 18:21:51 +0100

    Finale Version

--- Hook/Berechnung.py
+++ Hook/Berechnung.py
@@ -1,17 +1,25 @@
 
 
 import numpy as np
-s = 0
-i=0
-z=0
+
 
 x, F = np.genfromtxt('werte.txt', unpack=True)
 
 for c in x:
     D=F/x
     
+M = np.sum(D)/len(x)
+
+for k in D:
+    s = (D - M)**2
+
+sx = np.sqrt((1/9) * np.sum(s))
     
-print(np.sum(D)/len(x))
+print(M)
+print(s)
+print(sx)
+print(sx/np.sqrt(10))
+
 
 
 
