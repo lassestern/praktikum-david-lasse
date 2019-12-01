1b951917e88a909101ff21d856aee09cbffc13c8
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2019-12-01 01:04:49 +0100

    Sorry für das Chaos. Könne unnötige Sachen im Nachhinein löschen. Brauchte rel viele csv. Das sind jetzt alle Graphen mit lin Regression. Werte für E sind noch nicht dabei. Die versuche ich morgen zu machen.

---
+++ Stäbe/graphez.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+irrel, D_0, D_a, D, x = np.genfromtxt("ezwei.csv", delimiter=",", unpack = True)
+x_reg1, D_reg1 = np.genfromtxt("ezuhr1.csv", delimiter=",", unpack = True)
+x_reg2, D_reg2 = np.genfromtxt("ezuhr2.csv", delimiter=",", unpack = True)
+l_e = 0.55
+x_achs = l_e*x**2-x**3/3
+x_reg1a = l_e*x_reg1**2-x_reg1**3/3
+x_reg2a = l_e*x_reg2**2-x_reg2**3/3
+
+
+
+plt.plot(x_achs, D, "x", label =r"Graph Eckig zweiseitig")
+plt.xlabel(r"3L²x-4x³")
+plt.ylabel(r"D(x)")
+
+params_1, covariance_matrix_1 = np.polyfit(x_reg1a, D_reg1, deg = 1, cov = True)
+x_plot = np.linspace(0, 0.03)
+
+plt.plot(x_plot, params_1[0] * x_plot + params_1[1], label = r'Lineare Regression Uhr 1')
+
+
+
+params_2, covariance_matrix_2 = np.polyfit(x_reg2a, D_reg2, deg = 1, cov = True)
+
+plt.plot(x_plot, params_2[0] * x_plot + params_2[1], label = r'Lineare Regression Uhr 2')
+
+plt.legend(loc = 'best')
+
+plt.show()
+plt.savefig("ezwei.pdf")
+plt.close()
---
+++ Stäbe/graphre.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, D_0, D_a, D = np.genfromtxt("rein.csv", delimiter=",", unpack = True)
+l_e = 0.49
+x_achs = l_e*x**2-x**3/3
+
+
+plt.plot(x_achs, D, "x", label =r"Messwerte")
+plt.xlabel(r"3L²x-4x³")
+plt.ylabel(r"D(x)")
+
+params, covariance_matrix = np.polyfit(x_achs, D, deg = 1, cov = True)
+x_plot = np.linspace(0, 0.08)
+
+plt.plot(x_plot, params[0] * x_plot + params[1], label = r'Lineare Regression')
+
+plt.legend(loc = 'best')
+
+plt.show()
+plt.savefig("reins.pdf")
+plt.close()
+print(params[0], params[1])
