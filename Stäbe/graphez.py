1ed2706731f4868f41e45138d6b2e80340945000
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2019-12-02 20:48:10 +0100

    Rechnungen, die die richtigen E's ausgeben und auch die Fehler. Bei zweiseitiger Auflage habe ich zwei Regressionen und daher auch zwei A's bzw ein gemitteltes.

--- Stäbe/graphez.py
+++ Stäbe/graphez.py
@@ -13,11 +13,11 @@ x_reg2a = 3*l_e**2*x_reg2-4*x_reg2**3
 
 
 plt.plot(x_achs, D*10**(-3), "x", label =r"Graph Eckig zweiseitig")
-plt.xlabel(r"3L²x-4x³")
-plt.ylabel(r"D(x)")
+plt.xlabel(r"3L²x-4x³ [m³]")
+plt.ylabel(r"D(x) [m]")
 
 params_1, covariance_matrix_1 = np.polyfit(x_reg1a, D_reg1*10**(-3), deg = 1, cov = True)
-x_plot = np.linspace(0, 0.03)
+x_plot = np.linspace(0, 0.17)
 
 plt.plot(x_plot, params_1[0] * x_plot + params_1[1], label = r'Lineare Regression Uhr 1')
 
@@ -33,7 +33,7 @@ plt.show()
 plt.savefig("ezwei.pdf")
 plt.close()
 
-I=0.0122*0.0110**3/12
+I=0.0122**3*0.0110/12
 
 a_D = (params_1[0]+params_2[0])/2
 
@@ -45,9 +45,16 @@ errors_1 = np.sqrt(np.diag(covariance_matrix_1[0]))
 errors_2 = np.sqrt(np.diag(covariance_matrix_2[0]))
 delta_e= (0.0207+0.0189+1.1686+1.1597)*const.g /(48*I*a_D**2) * ((errors_1[0]+errors_2[0])/2)
 
+print('Elastizitätsmodul im Durchschnitt')
 print(E)
+print('Elastizitätsmodul von Uhr 1')
 print(E_1)
+print('Elastizitätsmodul von Uhr 2')
 print(E_2)
+print('Fehler von Uhr 1')
 print(errors_1)
+print('Fehler von Uhr 2')
 print(errors_2)
+print('Gemittelter Fehler')
 print(delta_e)
+print(params_1, params_2)
