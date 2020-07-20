cffd731fd3ef884ffba62583893fff53805d8a27
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2020-07-20 19:15:41 +0200

    Ich halte nichts von Ordnung nur von Faulheit

--- V601_FranckHertz/Auswertung.py
+++ V601_FranckHertz/Auswertung.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 
 #### mittlere freie Weglänge ###############################################################################################################
-w_30085 = 0.0029/(5.5*10**7*np.exp(-6876/300.85))       #mittlere freie Weglänge bei einer Temp von 300,85 Kelvin
-w_42415 = 0.0029/(5.5*10**7*np.exp(-6876/424.15))       #mittlere freie Weglänge bei einer Temp von 425,15 Kelvin
-w_44315 = 0.0029/(5.5*10**7*np.exp(-6876/443.15))       #mittlere freie Weglänge bei einer Temp von 443,15 Kelvin
-w_47025 = 0.0029/(5.5*10**7*np.exp(-6876/470.25))       #mittlere freie Weglänge bei einer Temp von 470,25 Kelvin
+w_30085 = 0.0029/(5.5*10**9*np.exp(-6876/300.85))       #mittlere freie Weglänge bei einer Temp von 300,85 Kelvin
+w_42415 = 0.0029/(5.5*10**9*np.exp(-6876/424.15))       #mittlere freie Weglänge bei einer Temp von 425,15 Kelvin
+w_44315 = 0.0029/(5.5*10**9*np.exp(-6876/443.15))       #mittlere freie Weglänge bei einer Temp von 443,15 Kelvin
+w_47025 = 0.0029/(5.5*10**9*np.exp(-6876/470.25))       #mittlere freie Weglänge bei einer Temp von 470,25 Kelvin
 
-rel_30085 = 0.01/w_30085                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 300,85 Kelvin
-rel_42415 = 0.01/w_42415                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 425,15 Kelvin
-rel_44315 = 0.01/w_44315                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 443,15 Kelvin
-rel_47025 = 0.01/w_47025                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 470,25 Kelvin
+rel_30085 = (0.01/w_30085)                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 300,85 Kelvin
+rel_42415 = (0.01/w_42415)                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 425,15 Kelvin
+rel_44315 = (0.01/w_44315)                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 443,15 Kelvin
+rel_47025 = (0.01/w_47025)                                #Verhältnis zwischen Abstand Glühdraht und Anode bei 470,25 Kelvin
 
 print(f"""
 mittlere freie Weglängen
@@ -29,16 +29,16 @@ Verhältnis a/w
 #### Aufgabenteil a ########################################################################################################################
 x_Skala1 = np.genfromtxt("x_Skala1.txt", unpack = True) 
 x_Skala2 = np.genfromtxt("x_Skala2.txt", unpack = True)
-U_Skala1 = (np.sum(5/x_Skala1)/len(x_Skala1))              #Volt pro Kästchen
-U_Skala2 = (np.sum(5/x_Skala2)/len(x_Skala2))              #Volt pro Kästchen
+U_Skala1 = (np.sum(1/x_Skala1)/len(x_Skala1))              #Volt pro Kästchen
+U_Skala2 = (np.sum(1/x_Skala2)/len(x_Skala2))              #Volt pro Kästchen
 
 deltay1, deltax1, xpos1 = np.genfromtxt("steigung1.txt", unpack = True)
 Steigung1 = deltay1/deltax1
 U1 = xpos1*U_Skala1
-
+print(Steigung1)
 plt.plot(U1, Steigung1, r'x', label=r"Betrag")
 plt.xlabel(r"Bremsspannung [V]")
-plt.ylabel(r"|Steigung|")
+plt.ylabel(r"|$\propto \frac{\Delta I}{\Delta U}$|")
 plt.legend(loc = "best")
 plt.savefig("Steigung1.pdf")
 plt.clf()
@@ -46,13 +46,18 @@ plt.clf()
 deltay2, deltax2, xpos2 = np.genfromtxt("steigung2.txt", unpack = True)
 Steigung2 = deltay2/deltax2 
 U2 = xpos2*U_Skala2
-print(Steigung2, deltay2)
+print(Steigung2)
 plt.plot(U2, Steigung2, r'gx', label=r"Betrag")
 plt.xlabel(r"Bremsspannung [V]")
-plt.ylabel(r"|Steigung|")
+plt.ylabel(r"|$\propto \frac{\Delta I}{\Delta U}$|")
 plt.legend(loc = "best")
 plt.savefig("Steigung2.pdf")
 plt.clf()
+
+print(f""" 
+Volt pro Kästchen 300,85K: {U_Skala1}
+Volt pro Kästchen 424,15K: {U_Skala2}
+""")
 
 
 
